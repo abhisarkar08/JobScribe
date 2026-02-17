a389b87c20bff649df9cc5b158b2f8384bbee80f
Abhishek <abhisarkar0805@gmail.com>
2026-02-17 21:19:43 +0530

    up

--- Frontend/src/Pages/Register/Register.jsx
+++ Frontend/src/Pages/Register/Register.jsx
@@ -3,7 +3,12 @@ import styles from './Register.module.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.page}>
+      <img src="img1.svg" className={`${styles.bg} ${styles.i1}`}></img>
+      <img src="img2.svg" className={`${styles.bg} ${styles.i2}`}></img>
+      <img src="img3.svg" className={`${styles.bg} ${styles.i3}`}></img>
+      <div className={styles.register}></div>
+    </div>
   )
 }
 
